072c57772d5c985d56a7411fc09c3d8bc4f29f16
BrotherMutton <52295709+BrotherMutton@users.noreply.github.com>
2019-07-30 14:08:07 -0400

    Cloning SpaceShooter files

---
+++ TheFinalProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public int winScore;
+
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+    public Text devText;
+
+    private bool gameOver;
+    private bool restart;
+    private bool gameWon;
+    private int Score;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        devText.text = "";
+        Score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (restart)
+        {
+            if(Input.GetKeyDown(KeyCode.Backspace))
+            {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)         
+        {
+            for(int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x),spawnValues.y,spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate (hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if(gameOver)
+            {
+                restartText.text = "Press 'Backspace' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore (int newScoreValue)
+    {
+        Score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Points: " + Score;
+        if(Score >= winScore)
+        {
+            gameWon = true;
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game over!";
+        gameOver = true;
+        if (gameWon == true)
+        {
+            gameOverText.text = "You've won!";
+            devText.text = "Game created by Matthew Wisner";
+        }
+    }
+}
